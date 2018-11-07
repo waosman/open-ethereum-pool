070056178ee1b76de9d5ddd05e25a601bbc542b8
A.O.B <42439722+waosman@users.noreply.github.com>
2018-11-07 14:53:55 +0300

    Update rpc.go

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -14,7 +14,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 
-	"github.com/techievee/open-ethereum-pool/util"
+	"github.com/waosman/open-ethereum-pool/util"
 )
 
 type RPCClient struct {
