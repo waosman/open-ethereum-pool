7bea11852996cabd61c17982afcdcc7352d585ab
A.O.B <42439722+waosman@users.noreply.github.com>
2018-11-07 15:01:04 +0300

    Update env.sh

--- build/env.sh
+++ build/env.sh
@@ -10,7 +10,7 @@ fi
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
 root="$PWD"
-ethdir="$workspace/src/github.com/techievee"
+ethdir="$workspace/src/github.com/waosman"
 if [ ! -L "$ethdir/open-ethereum-pool" ]; then
     mkdir -p "$ethdir"
     cd "$ethdir"
